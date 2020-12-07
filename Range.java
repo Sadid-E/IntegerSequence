0140d303947ebd2233cb3ef1ae2a6e9475229e70
Sadid-E <>
2020-12-06 19:47:55 -0500

    Updated constructor

--- Range.java
+++ Range.java
@@ -5,7 +5,11 @@ public class Range implements IntegerSequence{
 
   //@param start : the starting value (inclusive) which must be <= end.
   //@param end : the ending value which is also inclusive.
-  public Range(int start,  int end){  }
+  public Range(int start,  int end){
+    this.start = start;
+    this.end = end;
+    current = start; 
+  }
 
   public void reset(){  }
   public int length(){  }
